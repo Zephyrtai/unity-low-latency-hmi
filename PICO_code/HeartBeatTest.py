a62f1a475cbc3fc3e3f39c7fb251ceefe13c2868
DevUser <zephyrtai@gmail.com>
2026-05-20 23:37:45 +0300

    Clean comments and update repository structure

--- PICO_code/HeartBeatTest.py
+++ PICO_code/HeartBeatTest.py
@@ -8,20 +8,15 @@ import time
 import random
 import sys
 
-# 1. The Handshake
+# 1. Connect to wifi
 def wifi_connect():
     wifi_name = "Wi Believe I Can Fi"
     password = "63z5vg6w"
-    #安全性: 真实项目中，直接把 WiFi 密码写在代码里是不安全的，通常会放在一个单独的 config.py 文件中
 
-    # ①.创建“工作站模式”（连接路由器）开发板像手机一样连接路由器的信号
-    wlan = network.WLAN(network.STA_IF) # STA_IF：Station模式
-    # ②. Enable the network adapter
+    wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    # ③. Start connecting to WiFi
     wlan.connect(wifi_name, password)
-    # ④. 阻塞等待（重连） 确保网络通畅前代码不会往下运行 Block until connected
-    timeout = 10
+
     while not wlan.isconnected() and timeout > 0:
         print("Connecting to Wifi...")
         time.sleep(1)
@@ -38,9 +33,8 @@ UDP_IP = "192.168.101.8"
 UDP_PORT = 5005
 
 # Create a UDP Socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#AF_INT：IPv4:Address Family - Internet Protocol v4； SOCK.DGRAM:UDP Protocol
-sock.setblocking(False) # 【核心】设置为 Non-Blocking 非阻塞模式
-# ***【举一反三】: 以后遇到任何涉及外部设备 (网卡、蓝牙、串口)的操作，都要找找有没有 setblocking(False) 这个选项***
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setblocking(False) # set Non-Blocking mode
 
 # 3. Collect data
 # ①.Initialize
@@ -52,34 +46,32 @@ duration = 0
 last_dis = 0.0
 data_ready = False
 
-# Interrupt ReQuest: 当 Echo 引脚电平变化时，调用此中断回调函数 
-def echo_handler(pin): # 参数驱动
-    #global:告诉函数，此变量是在外面定义的，我要在函数内部修改它的值
+# Interrupt ReQuest: called when Echo pin changes state 
+def echo_handler(pin):
     global start_time, duration, data_ready
-# ***有跳变时，才会记录***
+
     if pin.value() == 1:
         start_time = time.ticks_us()
     else:
         end_time = time.ticks_us()
         duration = time.ticks_diff(end_time, start_time)
-        data_ready = True #【标志位】告诉主程序：数据写好了
+        data_ready = True
 
-# ②.【Bound Interrupt】: 关注上升沿和下降沿（IRQ_RISING | IRQ_FALLING）
+# ②.【Bound Interrupt】: Trigger on both rising and falling edges
 echo.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler = echo_handler)
 
-def trigger_sensor(): # 只负责触发
+def trigger_sensor():
     global data_ready
-    data_ready = False # 触发前先重置标志，确保读到的是“这次”的数据
+    data_ready = False
 
     # Trigger distance measurement
     launch.low()
-    time.sleep_us(2) # us：微秒
+    time.sleep_us(2)
     launch.high()
-    time.sleep_us(10) # Boost sensor
+    time.sleep_us(10)
     launch.low()
 
 # 4. The heartbeat (Send data)
-# 记录上次发送的 timestamp, 用于[控制频率]
 last_send_tick = time.ticks_ms()
 last_trigger_tick = time.ticks_ms()
 
@@ -88,38 +80,33 @@ wifi_connect()
 while True:
     current_tick = time.ticks_ms()
 
-    # 1. 周期性触发传感器(once every 100ms)
+    # 1. Trigger on sensor(once every 100ms)
     if time.ticks_diff(current_tick, last_trigger_tick) >= 200:
         trigger_sensor()
         last_trigger_tick = current_tick
 
     # 2. Check whether the data has been updated by Interrupt ReQuest
     if data_ready:
-        # 进入临界保护区: [保护共享变量**读取**]，防止被中断回调函数修改
-        # 专业术语：原子性保护的嵌套
-        state = machine.disable_irq() # 关掉中断“插队”功能，暂停保存状态
-        current_duration = duration #[使用本地变量进行计算（此时计算再久也不会被干扰）] **【加快速度，给完变量就退出。不用等待计算结果】**
+        state = machine.disable_irq()
+        current_duration = duration
         data_ready = False
-        machine.enable_irq(state) # 恢复中断(还原到暂停时的状态)
-        # **保证了无论进入函数前 中断是开还是关，退出函数后都会回到原来的样子**
+        machine.enable_irq(state)
 
         last_dis = current_duration * 0.034 / 2
 
-    # 3. 周期性发送网络数据 (不影响测距频率)
-    if time.ticks_diff(current_tick, last_send_tick) >= 200:# 20Hz 发送
-        # ①. Json format（包装成字典）
+    # 3. Send data
+    if time.ticks_diff(current_tick, last_send_tick) >= 200:# 20Hz
         data = {
             "speed": round(random.uniform(0,30), 2),
             "raw_distance": round(last_dis, 2)
         }
-        # ②. 序列化：将字典转化为字符串
+
         message = json.dumps(data)
-        # ③.【发送】:转换为字节流 .encode() {二进制字节(Bytes)} (无法直接发送字符串)
+       
         try:
             sock.sendto(message.encode(), (UDP_IP, UDP_PORT))
             print("Sent:", message)
-        except Exception as e:  # 对应上面 setblocking(False)，捕获网络异常（如网络波动导致发送失败）
-            #如果网络发不出去，没关系，直接跳过，打印提醒或干脆保持沉默
-            #保证下一行测距代码能 按时运行
+        except Exception as e:
+            
             print("Network bussy, skipping this packet.")
-            pass  # 避免网络波动卡死主循环
+            pass
--- Thesis_Unity_Project/Assets/Script/UDPReceiver.cs
+++ Thesis_Unity_Project/Assets/Script/UDPReceiver.cs
@@ -5,10 +5,8 @@ using System.Text;
 using System.Threading;
 using UnityEngine;
 
-/// <summary>
-/// 
-/// </summary>
-[Serializable] //To make data can be packaged
+
+[Serializable]
 public class SensorData
 {
     public float speed;
@@ -29,7 +27,7 @@ public class UDPReceiver : MonoBehaviour
             Debug.LogWarning("Receiver is already running.");
             return;
         }
-        //1. Create a new Thread to execute
+        //1. Create a new Thread
         receiveThread = new Thread(new ThreadStart(ReceiveData));
         //2. Set background mode to prevent from being unable to close
         receiveThread.IsBackground = true; //The default is foreground thread
@@ -40,28 +38,21 @@ public class UDPReceiver : MonoBehaviour
     private void ReceiveData()
     {  //1. Create client
         if (client == null) client = new UdpClient(port);
-        /* 允许端口复用（避免偶发冲突） */
-        /* client.Client.SetSocketOption(
-           SocketOptionLevel.Socket,  //表示要设置的是 通用的套接字层级选项。
-           SocketOptionName.ReuseAddress,  //选项名称, 允许“多个程序同时听同一个端口”
-            true  //启用功能 ); 
-        放在 Bind() 前面。*/
+        
         while (isRunning)
         {
             try 
             {  //2. Ready to receive data
-                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0); //Create to record (sender's)'IPAddress' and 'port'
-                byte[] data = client.Receive(ref anyIP);//【ref】:当数据包到达时，Receive 方法不仅会把数据拿回来，还会把sender的真实 IP 和port写进 anyIP 里
-            //阻塞等待（死等）：这行代码运行到这里会“卡住”，直到真的有人给你发了一个 UDP 包。如果没人发数据，程序就会一直停在这里等
-
-               //3. Parse 解析
-                string text = Encoding.UTF8.GetString(data);//Serder serializable data,correspondingly parse the data to read
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+               //3. Parse
+                string text = Encoding.UTF8.GetString(data);
                //4. read data
                 SensorData sensor = JsonUtility.FromJson<SensorData>(text);
                 Debug.Log($"speed: {sensor.speed} | raw distance: {sensor.raw_distance}");
             }
             catch (SocketException e)
-            {  // 重点：当 Socket 被关闭时，这里会接住那个 0x80004005 报错
+            {  
                 if (isRunning) Debug.LogWarning($"UDP receive exception {e.Message}");
                 else Debug.Log("UDP has been shutdown normally.");
             }
@@ -72,19 +63,18 @@ public class UDPReceiver : MonoBehaviour
         }
     }
 
-    private void OnApplicationQuit() //【message】(Unity)生命周期
+    private void OnApplicationQuit()
     {   
-        isRunning = false;// Stop receiving data(until next time start up)
+        isRunning = false;
 
         //1. if client exists, close it for releasing port to prevent errors caused by the port being occupaid
         if (client != null) {
             client.Close();
             client = null;
         }
-        //2. If thread exists, `.Join()` wait for the thread to finish normally
         if (receiveThread != null) {
             receiveThread.Join();
-            receiveThread = null; //Fully release upon exit(to avoid conflicts next time)
+            receiveThread = null;
         }
     }
 }
